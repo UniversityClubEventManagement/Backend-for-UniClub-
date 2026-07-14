9d95806a24a20e8460e3f7b07d48a12af425e8ef
Sachiisilva <sachisilva0301@gmail.com>
2026-07-15 04:20:22 +0530

    Add student profile active status during registration

--- models/User.js
+++ models/User.js
@@ -40,6 +40,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       trim: true,
     },
+    isActive: {
+   type: Boolean,
+   default: true,
+    },
   },
   {
     timestamps: true,
